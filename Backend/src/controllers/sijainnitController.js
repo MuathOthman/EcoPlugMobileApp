295ea02c00f9eb4e5fd09377c131f755cb52c2f4
isakovero <isakovero@gmail.com>
2024-02-06 17:52:35 +0200

    turns mysql to json

--- Backend/src/controllers/sijainnitController.js
+++ Backend/src/controllers/sijainnitController.js
@@ -1,12 +1,16 @@
 const sijainnitModel = require('../models/sijainnitModels');
+const connection = require('../config/db');
 
 // Get all sijainnit
 exports.findAll = (req, res) => {
-    sijainnitModel.findAll((err, rows) => {
+    connection.query('SELECT * FROM Sijainti', (err, rows) => {
         if (err) {
             res.status(500).send({ message: err.message || 'Some error occurred while retrieving sijainnit.' });
         } else {
-            res.send(rows);
+            console.log(`Retrieved ${rows.length} sijainni records:`);
+            console.table(rows);
+
+            res.json(rows);
         }
     });
 };
