498d861afcd537f24ab62415a1e2b8783bc0b9a8
agrin <agrin.sadon@metropolia.fi>
2024-02-21 15:36:44 +0200

    Changed Backend directory place

--- Backend/src/routes/otpRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const otpController = require('../controllers/otpController');
-
-
-router.post('/send-otp', otpController.sendOTP);
-router.post('/verify-otp', otpController.verifyOTP);
-module.exports = router;
