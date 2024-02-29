d6f629d173300da868d3cbe7ea4b42b8ebbe3a44
isakovero <isakovero@gmail.com>
2024-02-29 13:53:06 +0200

    added ecoplug welcome page to app.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,5 +1,5 @@
 // App.js (or your main entry point)
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MapScreen from './Components/Map'; // Replace with your actual screen components
@@ -8,25 +8,62 @@ import ParkingScreen from './Components/ParkingScreen'; // Replace with your act
 import ReservationScreen from './Components/Reservation'; // Replace with your actual screen components
 import CodeConfirm from "./Components/CodeConfirm";
 import Charging from "./Components/Charging";
+import WelcomePage from './Pages/WelcomePage'; // Import the WelcomePage component
+
 
 
 const Stack = createStackNavigator();
 
 const App = () => {
+    const [showWelcomePage, setShowWelcomePage] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowWelcomePage(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Map">
-                <Stack.Screen name="Map" component={MapScreen} options={{ tabBarLabel: '', headerShown: false }}
+            <Stack.Navigator initialRouteName={showWelcomePage ? 'Welcome' : 'Map'}>
+                {showWelcomePage && (
+                    <Stack.Screen
+                        name="Welcome"
+                        component={WelcomePage}
+                        options={{ headerShown: false }}
+                    />
+                )}
+                <Stack.Screen
+                    name="Map"
+                    component={MapScreen}
+                    options={{ tabBarLabel: '', headerShown: false }}
                 />
-                <Stack.Screen name="SearchPage" component={SearchPage} options={{ tabBarLabel: '', headerShown: false }}
+                <Stack.Screen
+                    name="SearchPage"
+                    component={SearchPage}
+                    options={{ tabBarLabel: '', headerShown: false }}
                 />
-                <Stack.Screen name="ParkingScreen" component={ParkingScreen} options={{ tabBarLabel: '', headerShown: false }}
+                <Stack.Screen
+                    name="ParkingScreen"
+                    component={ParkingScreen}
+                    options={{ tabBarLabel: '', headerShown: false }}
                 />
-                <Stack.Screen name="Reservation" component={ReservationScreen}  options={{ tabBarLabel: '', headerShown: false }}
+                <Stack.Screen
+                    name="Reservation"
+                    component={ReservationScreen}
+                    options={{ tabBarLabel: '', headerShown: false }}
                 />
-                <Stack.Screen name={"Verify"} component={CodeConfirm} options={{ tabBarLabel: '', headerShown: false }}
+                <Stack.Screen
+                    name="Verify"
+                    component={CodeConfirm}
+                    options={{ tabBarLabel: '', headerShown: false }}
                 />
-                <Stack.Screen name={"Charging"} component={Charging} options={{ tabBarLabel: '', headerShown: false }}
+                <Stack.Screen
+                    name="Charging"
+                    component={Charging}
+                    options={{ tabBarLabel: '', headerShown: false }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
