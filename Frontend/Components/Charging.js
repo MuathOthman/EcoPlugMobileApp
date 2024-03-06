2783f735ec7e9b000a4d9e1263744150e849a5d7
agrin <agrin.sadon@metropolia.fi>
2024-03-06 14:00:34 +0200

    Finishing touches now the usability of the project should work until further notice

--- Frontend/Components/Charging.js
+++ Frontend/Components/Charging.js
@@ -126,6 +126,7 @@ export default function Charging() {
                   latausID,
                   chargingTime,
                   totalCost,
+                  phoneNumber,
                }),
              });
 
